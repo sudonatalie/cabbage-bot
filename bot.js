5cbba28302621b1a95d7f744c4546197e8e76169
Natalie Chouinard <chouinard.nm@gmail.com>
2018-10-03 21:54:41 -0700

    Comments

--- bot.js
+++ bot.js
@@ -4,21 +4,27 @@ const bot = new Discord.Client();
 const GoogleImages = require('google-images');
 const searchClient = new GoogleImages(process.env.GOOGLE_CSE_ID, process.env.GOOGLE_API_KEY);
 
-const randomImage = function(searchResults) {
-  const urls = searchResults.map(image => image.url)
-      .filter(url => (/\.(gif|jpg|jpeg|png)$/i).test(url));
-  const url = urls[Math.floor(Math.random() * urls.length)];
-  return url;
+const randomImage = function(imageSearchResults) {
+  const urls = imageSearchResults
+    // Get urls
+    .map(image => image.url)
+    // Filter to files with extensions that Discord recognizes
+    .filter(url => (/\.(gif|jpg|jpeg|png)$/i).test(url));
+  // Select one randomly
+  return urls[Math.floor(Math.random() * urls.length)];
 };
 
 bot.on('message', message => {
+  // !cabbagebot
   if (message.content === '!cabbagebot') {
+
     // If the message is from cabbagebot, freak out
     if (message.author.username === 'cabbagebot') {
       message.reply({
         file: 'https://i.imgur.com/U5EJjfG.jpg'
       });
     }
+    // Otherwise reply with a cabbage image
     else {
       // Search for images
       searchClient.search('cabbage')
@@ -30,7 +36,10 @@ bot.on('message', message => {
         }));
     }
   }
+
+  // !mrscabbagebot
   else if (message.content === '!mrscabbagebot') {
+    // Reply with a banana image
     // Search for images
     searchClient.search('banana')
       // Choose a random image from the list
