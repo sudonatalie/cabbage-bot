2e980874fc0279c2207ee06035fd9f8ea593f336
Natalie Chouinard <chouinard.nm@gmail.com>
2018-10-20 21:19:47 -0700

    Add highly advanced artificial cabbage intelligence

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js');
+const GoogleImages = require('google-images');
+const Snoowrap = require('snoowrap');
+const Markov = require('markov-strings');
+
 const bot = new Discord.Client();
 
-const GoogleImages = require('google-images');
 const searchClient = new GoogleImages(process.env.GOOGLE_CSE_ID, process.env.GOOGLE_API_KEY);
+
+const reddit = new Snoowrap({
+  userAgent: 'Cabbagebot/1.0',
+  clientId: process.env.REDDIT_CLIENT_ID,
+  clientSecret: process.env.REDDIT_CLIENT_SECRET,
+  refreshToken: process.env.REDDIT_REFRESH_TOKEN
+});
 
 const randomImage = function(imageSearchResults) {
   const urls = imageSearchResults
@@ -74,6 +84,51 @@ bot.on('message', message => {
     });
   }
 
+  // !cabbageai
+  else if (message.content === '!cabbageai') {
+    // Search for "cabbage" posts on Reddit
+    const cabbagePromise = reddit.search({
+        query: 'title:cabbage',
+        time: 'all',
+        sort: 'relevance',
+        limit: 500,
+        syntax: 'lucene'
+      });
+
+    // Fetch the hot posts from the FortNiteBR subreddit
+    const fortnitePromise = reddit.getSubreddit('FortNiteBR')
+      .getHot({
+        limit: 500
+      });
+
+    // Combine both results
+    Promise.all([cabbagePromise, fortnitePromise])
+      .then(bothResults => {
+        const listings = bothResults[0].concat(bothResults[1]);
+
+        // Filter to self posts and construct list of their title/body texts
+        const texts = listings.reduce((accumulator, listing) => {
+          if (listing.is_self) {
+             accumulator.push(`${listing.title}\n${listing.selftext}`);
+          }
+          return accumulator;
+        }, []);
+
+        // Initialize Markov chain text generator
+        const markov = new Markov(texts);
+        markov.buildCorpus().then(() => {
+          // Generate a post
+          markov.generateSentence().then(result => {
+            var raw = result.string;
+            // Minor cleanup of garbage entities/whitespace
+            var response = raw.replace(/&#x200B;/g, '').replace(/\n\s*\n+/g, '\n');
+            // Send reply on Discord
+            message.reply(response);
+          });
+        });
+      })
+      .catch(error => console.log(error));
+  }
 });
 
 bot.login(process.env.DISCORD_TOKEN);
